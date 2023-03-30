5f64178954e699565a1313967878ecd431a007f6
Moss <charlesiddle99@gmail.com>
2023-03-30 05:51:22 +0300

    puppet install nginx

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,10 @@
+#This manifest configures a web server using puppet to run nginx
+
+package {'nginx':
+  ensure => installed
+}
+
+file {'/var/www/html/index.html':
+  ensure => file,
+  content => 'Hello World!'
+}
