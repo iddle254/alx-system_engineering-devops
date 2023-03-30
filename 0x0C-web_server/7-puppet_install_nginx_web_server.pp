40dee611cd6ee68c4e0943996688db1918a4350e
Moss <charlesiddle99@gmail.com>
2023-03-30 05:55:35 +0300

    redirect

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,10 +1,25 @@
 #This manifest configures a web server using puppet to run nginx
 
+exec { 'update system':
+        command => '/usr/bin/apt-get update',
+}
+
 package {'nginx':
-  ensure => installed
+  ensure => installed,
+  require => Exec['update system']
 }
 
 file {'/var/www/html/index.html':
   ensure => file,
   content => 'Hello World!'
+}
+
+exec {'redirect_me':
+	command => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
+	provider => 'shell'
+}
+
+service {'nginx':
+	ensure => running,
+	require => Package['nginx']
 }
